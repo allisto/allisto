457ca1520c800b5ce7c1f3fdbce95dfd47cc71f6
rshrc <rishieric91@gmail.com>
2018-11-04 02:59:22 +0530

    Made the main.py partially work

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-from model import Allisto
+from model import Allisto, AllistoPredict
+from audio_analyzer import AudioAnalyze
+import numpy as np
+
+analyzed_audio = AudioAnalyze("sample.wav")
+print("Audio Analysis Complete..")
+
+allisto_prediction = AllistoPredict()
+allisto_prediction.predict(analyzed_audio.slice_audio_parameters())
+
+print(allisto_prediction)
