c8e17c7f7facce0abcbbd3e66e44c6727d914053
rshrc <rishieric91@gmail.com>
2018-11-04 02:40:10 +0530

    AudioAnalyzer Made
    Signed-off-by: rshrc <rishieric91@gmail.com>

--- audio_analyzer.py
+++ audio_analyzer.py
@@ -2,6 +2,18 @@ from pyAudioAnalysis import audioBasicIO
 from pyAudioAnalysis import audioFeatureExtraction
 import numpy as np
 
-# Reading the audio file
-[Fs, x] = audioBasicIO.readAudioFile("sample.wav")
-F = audioFeatureExtraction.stFeatureExtraction(np.mean(x, axis=1),Fs, 0.050*Fs, 0.025*Fs)
+class AudioAnalyze:
+    
+    def __init__(self, filename):
+        [Fs, x] = audioBasicIO.readAudioFile(filename)
+        F = audioFeatureExtraction.stFeatureExtraction(np.mean(x, axis=1),Fs, 0.050*Fs, 0.025*Fs)
+        self.input_from_audio = F[0][1]
+    
+    def slice_audio_parameters(self):
+        value = self.input_from_audio[:19]
+        return value
+        
+"""
+obj = AudioAnalyze("sample.wav")
+value = obj.slice_audio_parameters()
+"""
