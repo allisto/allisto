294ad45a3fc07dac786ecb914d500541151d69e4
Nedheesh Hasija <nedheesh.hasija@gmail.com>
2019-02-26 12:28:51 +0530

    New audio to spectrogram conversion file.
    audio.py takes in a sample wav file and gives a spectrogram png file in the same folder

--- ai_and_audio/audio.py
+++ ai_and_audio/audio.py
@@ -1,11 +1,10 @@
-import os
 import wave
 import pylab
 
 
 def graph_spectrogram(wav_file):
     sound_info, frame_rate = get_wav_info(wav_file)
-    pylab.figure(num=None, figsize=(19, 12))
+    pylab.figure(num=None, figsize=(19,12))
     pylab.subplot(111)
     pylab.title('spectrogram of %r' % wav_file)
     pylab.specgram(sound_info, Fs=frame_rate)
@@ -13,12 +12,13 @@ def graph_spectrogram(wav_file):
 
 
 def get_wav_info(wav_file):
-    wav = wave.open(wav_file, 'r')
+    wav = wave.open("sample.wav", "r")
     frames = wav.readframes(-1)
     sound_info = pylab.fromstring(frames, 'Int16')
     frame_rate = wav.getframerate()
     wav.close()
-    return sound_info, frame_rate
+
+    return sound_info,frame_rate
 
 
 if __name__ == '__main__':
